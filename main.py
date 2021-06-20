aa0576923c0dedf3c14768f938b3c17123776ac6
amprusty <amrit2995@gmail.com>
2021-06-20 14:36:23 +0530

    Final Basic Structure

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# create 3*3 matrix
-# UI class
-# user entry
-# computer entry
-import random
-board = []
-symbols = {'O': 0, 'X': 1}
-# board = [[' ' for _ in range(3)] for _ in range(3)]
-
-
 class Board:
     player1 = None
     player2 = None
@@ -15,7 +5,7 @@ class Board:
 
     def board_print(self):
         for i in range(3):
-            print(f"{board[i][0]} | {board[i][1]} | {board[i][2]}")
+            print(f"{self.board[i][0]} | {self.board[i][1]} | {self.board[i][2]}")
             if i < 2:
                 print('-' * 11)
 
@@ -32,54 +22,66 @@ class Board:
                 return
 
     def user_input(self, player_inp):
-        player = self.player1 if player_inp == 1 else self.player2
-        pos = 0
-        while pos not in range(1, 10):
-            pos = input(f"Input position for Player{player_inp}:\n")
-            if pos.isalnum():
-                pos = int(pos)
-            if pos in range(1, 10):
-                pos = int(pos)
-                board[(pos - 1)//3][(pos - 1) % 3] = player
-                self.board_print()
-                return
+        self.valid_pos(player_inp)
+        self.board_print()
+        return
 
-    def validate_victory(self, player):
-        player = self.player1 if player == 1 else self.player2
+    def valid_pos(self, player):
+        valid = False
+        while not valid:
+            pos = input(f"Enter a  pos for {player} :\n")
+            if not pos.isnumeric():
+                print("Position should be a number ")
+            elif int(pos) not in range(1, 10):
+                print("Position should be within range 1 to 9 ")
+            else:
+                i = (int(pos)-1)//3
+                j = (int(pos)-1) % 3
+                if self.board[i][j] == ' ':
+                    self.board[i][j] = self.player1 if player == 1 else self.player2
+                    return
+                else:
+                    print("This position is already occupied.")
+
+    def validate_victory(self, player_num):
+        player = self.player1 if player_num == 1 else self.player2
         for i in range(3):
-            row_v = "".join([board[i][j] for j in range(3)])
-            col_v = "".join([board[j][i] for j in range(3)])
+            row_v = "".join([self.board[i][j] for j in range(3)])
+            col_v = "".join([self.board[j][i] for j in range(3)])
             if player*3 in [row_v, col_v]:
+                print(f"Player{player_num} won !!!")
                 return True
-        if player*3 in ["".join(board[i][i] for i in range(3)), "".join(board[i][2-i] for i in range(3))]:
+        if player*3 in ["".join(self.board[i][i] for i in range(3)), "".join(self.board[i][2-i] for i in range(3))]:
+            print(f"Player{player_num} won !!!")
             return True
 
     def board_full(self):
         for i in range(3):
             for j in range(3):
-                if board[i][j] == ' ':
-                    return True
-        return False
+                if self.board[i][j] == ' ':
+                    return False
+        print("Draw !!!")
+        return True
+
+    def reset_board(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
 
 if __name__ == "__main__":
     boardgame = Board()
     play_again = "y"
+    # bot = input("Do you want to play with a bot ?: \n")
+    #
     while play_again == 'y':
-        reset_board()
+        boardgame.reset_board()
         vic1, vic2 = False, False
         boardgame.symbol_choice()
         while not (vic1 and vic2):
             # boardgame.board_print()
             boardgame.user_input(1)
-            if boardgame.validate_victory(1) :
-                print('Player1 Wins')
+            if boardgame.validate_victory(1) or boardgame.board_full() :
                 break
             boardgame.user_input(2)
-            if boardgame.validate_victory(2):
-                print('Player2 Wins')
-                break
-            if not boardgame.board_full():
-                print("Game Draw!")
+            if boardgame.validate_victory(2) or boardgame.board_full():
                 break
         play_again = input("Do you want to continue:(y or n) \n").lower()
