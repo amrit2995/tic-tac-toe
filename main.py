746ebe87f6e922e3c7fff61c7e0eb5ac06009071
amprusty <amrit2995@gmail.com>
2021-06-20 15:08:47 +0530

    This is After removing the choice option

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Board:
-    player1 = None
-    player2 = None
+    player1 = 'X'
+    player2 = 'O'
+    bot = False
     board = [[' ' for _ in range(3)] for _ in range(3)]
 
     def board_print(self):
@@ -9,39 +10,26 @@ class Board:
             if i < 2:
                 print('-' * 11)
 
-    def symbol_choice(self):
-        choice = None
-        count = 0
-        while choice not in ["X", "O"]:
-            choice = input('Choose the Symbol for Player1 : \n').upper()
-            if choice == 'X':
-                self.player1, self.player2 = ['X', 'O']
-                return
-            elif choice == 'O':
-                self.player1, self.player2 = ['O', 'X']
-                return
-
     def user_input(self, player_inp):
-        self.valid_pos(player_inp)
-        self.board_print()
-        return
+        pos = input(f"Enter a  pos for Player{player_inp} :\n")
+        if self.valid_pos(pos):
+            self.board[(int(pos)-1)//3][(int(pos)-1) % 3] = "X" if player_inp == 1 else "O"
+            self.board_print()
+        else:
+            self.user_input(player_inp)
 
-    def valid_pos(self, player):
-        valid = False
-        while not valid:
-            pos = input(f"Enter a  pos for {player} :\n")
-            if not pos.isnumeric():
-                print("Position should be a number ")
-            elif int(pos) not in range(1, 10):
-                print("Position should be within range 1 to 9 ")
+    def valid_pos(self, pos):
+        if not pos.isnumeric():
+            print("Position should be a number ")
+        elif int(pos) not in range(1, 10):
+            print("Position should be within range 1 to 9 ")
+        else:
+            i = (int(pos) - 1) // 3
+            j = (int(pos) - 1) % 3
+            if self.board[i][j] == ' ':
+                return True
             else:
-                i = (int(pos)-1)//3
-                j = (int(pos)-1) % 3
-                if self.board[i][j] == ' ':
-                    self.board[i][j] = self.player1 if player == 1 else self.player2
-                    return
-                else:
-                    print("This position is already occupied.")
+                print("This position is already occupied.")
 
     def validate_victory(self, player_num):
         player = self.player1 if player_num == 1 else self.player2
@@ -75,7 +63,7 @@ if __name__ == "__main__":
     while play_again == 'y':
         boardgame.reset_board()
         vic1, vic2 = False, False
-        boardgame.symbol_choice()
+        # boardgame.symbol_choice()
         while not (vic1 and vic2):
             # boardgame.board_print()
             boardgame.user_input(1)
